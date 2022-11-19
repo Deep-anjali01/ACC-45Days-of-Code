3634714130a768542672792e93379ee9f7118089
DEEPANJALIUPADHYAY <upadhyaydeepanjali@gmail.com>
2022-11-19 23:45:32 +0530

    Day 26

---
+++ Day26.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,a;
+    cin>>n;
+    
+    
+    if(n%2==0 && n!=2)cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+}

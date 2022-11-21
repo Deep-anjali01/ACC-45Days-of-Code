36049271b7669a5d39c30ddfdd76d9bd3caaf0d3
DEEPANJALIUPADHYAY <upadhyaydeepanjali@gmail.com>
2022-11-21 22:17:58 +0530

    Day 28

--- DAy28.cpp
+++ DAy28.cpp
@@ -1,24 +1,3 @@
-// #include <iostream>
-// using namespace std;
-
-// int main() {
-// 	// your code goes here
-// 	int t;
-// 	cin>>t;
-// 	while(t--)
-// 	{
-//         int a,b,c;
-//         cin>>a>>b>>c;
-
-//         if(a>b && b>c || c>b && b>a)
-//         cout<<b<<endl;
-//         else if(b>c && c>a || a>c && c>b)
-//         cout<<c<<endl;
-
-//         else if(b>a && a>c || c>a && a>b)
-//         cout<<a<<endl;
-//     }
-// }
 
 
 #include <iostream>

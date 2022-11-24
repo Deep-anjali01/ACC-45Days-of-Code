967b0a0107d5bd68c5b31e3bf4481cdfe4b31863
DEEPANJALIUPADHYAY <upadhyaydeepanjali@gmail.com>
2022-11-24 22:29:08 +0530

    Day 32

--- Day31.cpp
+++ Day31.cpp
@@ -1,21 +1,3 @@
-// #include <iostream>
-// using namespace std;
-
-// int main() {
-// 	// your code goes here
-//     int t;
-//     cin>>t;
-//     while (t--) {
-//         int a,b,c;
-//         cin>>a>>b>>c;
-//         if(a+b<c ||a+c<b ||c+b<a)
-//         cout<<"YES"<<endl;else cout<<"NO"<<endl;
-
-
-//     }
-	
-// 	return 0;
-// }
 #include <iostream>
 
 #include<bits/stdc++.h>
@@ -30,25 +12,15 @@ int main() {
         cin>>n;
         int a,b;
         cin>>a>>b;
-        c=sqrt(n);n=c;
-        while(c%2==0){
-            c=sqrt(n);
-        }
-        cout<<c%1000000007<<endl;
-        c=2*c;
-        while(c>0){
-            sum=sum+a+b;
-            c--;
-            
-
-        }
       
+        c=log2(n);
        
 
-        cout<<sum-b<<endl;
+        cout<<c*a+(c-1)*b<<endl;
 
     }
 	
 	return 0;
 }
+
 
